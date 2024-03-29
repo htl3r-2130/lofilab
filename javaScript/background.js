bbec672eb4b15876b2415e2f396c7165aadcd78b
Linus Bittner <2130@htl.rennweg.at>
2024-03-29 10:18:50 +0100

    background img update

--- javaScript/background.js
+++ javaScript/background.js
@@ -6,17 +6,17 @@ const img3 = document.getElementById("img3");
 const img4 = document.getElementById("img4");
 
 img1.addEventListener("click", function(){
-     container.style.backgroundImage = "url(resources/pics/pexels-james-wheeler-417074.jpg)";
+     container.style.backgroundImage = "url(resources/pics/pexels-james-wheeler-417074_1920px.jpg)";
 });
 
 img2.addEventListener("click", function(){
-     container.style.backgroundImage = "url(resources/pics/pexels-pixabay-33109.jpg)";
+     container.style.backgroundImage = "url(resources/pics/pexels-pixabay-33109_1920px.jpg)";
 });
 
 img3.addEventListener("click", function(){
-     container.style.backgroundImage = "url(resources/pics/pexels-pixabay-50594.jpg)";
+     container.style.backgroundImage = "url(resources/pics/pexels-pixabay-50594_1920px.jpg)";
 });
 
 img4.addEventListener("click", function(){
-     container.style.backgroundImage = "url(resources/pics/pexels-veeterzy-39811.jpg)";
+     container.style.backgroundImage = "url(resources/pics/pexels-veeterzy-39811_1920px.jpg)";
 });
